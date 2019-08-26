0f8cf60ae7683b56f88223b3436b362e87f5e01e
DIVYESH\divye <divyeshg@cloudassert.com>
2019-08-26 22:29:25 +0530

    particles.js introduced

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParticlesConfig } from './shared/particles.config';
+
+declare let particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'Bg Design Systems';
+  public ngOnInit(): void {
+    this.invokeParticles();
+  }
+
+  public invokeParticles(): void {
+    particlesJS('particles-js', ParticlesConfig, function() {});
+  }
 }
